0c39a7366f0f599d52214d6264a024ca5ec63cf0
pei.liao <pei.liao@yitu-inc.com>
2020-09-25 20:36:56 +0800

    add PROBLEMS show

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { clearDecorations, detectChineseWords } from './task';
+import { clearDecorations, detectChineseWordsinTextEditor, detectChineseWordsinWorkspace } from './task';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -12,42 +12,58 @@ export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "charset-detector" is now active!');
 
   let disable = true;
-  let _watch: vscode.Disposable;
-  const activeTextEditor = vscode.window.activeTextEditor;
-  if (!activeTextEditor) {
-    // Display a message box to the user
-    vscode.window.showInformationMessage('There is no open file!');
-    return;
-  }
-
+  let _watch: vscode.Disposable[];
+  const collections = vscode.languages.createDiagnosticCollection('Detector');;
   // generate a disposable to watch text change
-  const watch = () => vscode.workspace.onDidChangeTextDocument(() => {
-    detectChineseWords(activeTextEditor);
-  });
+  const watch = () => {
+    return [
+      vscode.workspace.onDidChangeTextDocument(() => {
+        detectChineseWordsinTextEditor(vscode.window.activeTextEditor);
+      }),
+      vscode.window.onDidChangeActiveTextEditor(() => {
+        detectChineseWordsinTextEditor(vscode.window.activeTextEditor);
+      }),
+    ];
+  };
 
   // activate extension 
   const activate = () => {
+
     // new a listener and subscribe it 
     _watch = watch();
-    context.subscriptions.push(_watch);
-    // 
-    detectChineseWords(activeTextEditor);
+    context.subscriptions.push(..._watch);
     disable = false;
+
+    const activeTextEditor = vscode.window.activeTextEditor;
+    if (!activeTextEditor) {
+      // Display a message box to the user
+      vscode.window.showInformationMessage('There is no open file!');
+      return;
+    }
+    // 
+    detectChineseWordsinTextEditor(vscode.window.activeTextEditor);
   };
 
   // deativate extension
   const deativate = () => {
+
     // unsubscribe the listener
-    _watch.dispose();
-    //
-    clearDecorations(activeTextEditor);
+    _watch.forEach((watch) => watch.dispose());
     disable = true;
+
+    //
+    const activeTextEditor = vscode.window.activeTextEditor;
+    if (!activeTextEditor) {
+      return;
+    }
+    clearDecorations(activeTextEditor);
+
   };
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let start = vscode.commands.registerTextEditorCommand('charset-detector.enableHightlight', () => {
+  let _enable = vscode.commands.registerCommand('charset-detector.enableHightlight', () => {
     // The code you place here will be executed every time your command is executed
 
     if (disable) {
@@ -58,15 +74,35 @@ export function activate(context: vscode.ExtensionContext) {
 
   });
 
-  let end = vscode.commands.registerTextEditorCommand('charset-detector.disableHightlight', () => {
+  let _disable = vscode.commands.registerCommand('charset-detector.disableHightlight', () => {
     if (!disable) {
       deativate();
     }
   });
 
-  context.subscriptions.push(start);
-  context.subscriptions.push(end);
+  let _run = vscode.commands.registerCommand('charset-detector.detectWorkspace', () => {
+    const projects = vscode.workspace.workspaceFolders;
+    if (!projects) {
+      vscode.window.showInformationMessage('No file found.');
+      return;
+    }
+    collections.clear();
+    if (projects.length > 1) {
+      vscode.window.showQuickPick(projects.map((p) => p.name)).then((name) => {
+        if (name) {
+          const project = projects.find((p) => p.name === name);
+          detectChineseWordsinWorkspace(collections, project!);
+        }
+      });
+    } else {
+      detectChineseWordsinWorkspace(collections, projects[0]);
+    }
+    console.log(projects);
+  });
 
+  context.subscriptions.push(_enable);
+  context.subscriptions.push(_disable);
+  context.subscriptions.push(_run);
 }
 
 // this method is called when your extension is deactivated
--- src/task.ts
+++ src/task.ts
@@ -1,24 +1,84 @@
 import * as vscode from 'vscode';
-import { textEditorDecorationTypeHighlight } from './config';
+import { DocumentDetectResult, ExtensionConfig, textEditorDecorationTypeHighlight } from './config';
 import { CHINESE_WORDS_REGEX } from './constants';
 
-export const detectChineseWords = (textEditor: vscode.TextEditor | undefined) => {
+export const createMessage = (textEditor: vscode.TextEditor | undefined) => {
   if (!textEditor) {
     // Display a message box to the user
-    vscode.window.showInformationMessage('No active file from charset-detector!');
+    vscode.window.showInformationMessage('There is no open file!');
+    return;
+  }
+
+
+};
+
+export const detectChineseWordsinWorkspace = (collection: vscode.DiagnosticCollection, project: vscode.WorkspaceFolder) => {
+  getWorkspaceConfig();
+  const projectDocuments = vscode.workspace.textDocuments.filter(doc => doc.fileName.includes(project.uri.fsPath));
+  console.log(projectDocuments[0]);
+  const visibleDocuments = vscode.window.visibleTextEditors.map((editor) => editor.document);
+  createResultDiagnosticCollection(collection, visibleDocuments);
+};
+
+const getWorkspaceConfig = () => {
+  const extensions = vscode.workspace.getConfiguration('charset-detector.extensions');
+  const ignore = vscode.workspace.getConfiguration('charset-detector.ignore');
+  const en = vscode.workspace.getConfiguration('charset-detector.wordlib.en');
+  const zh = vscode.workspace.getConfiguration('charset-detector.wordlib.zh');
+  return { extensions, ignore, wordlib: { en, zh } };
+};
+
+
+const createResultDiagnosticCollection = (collection: vscode.DiagnosticCollection, documents: vscode.TextDocument[]) => {
+
+  documents.forEach((document) => {
+    collection.set(document.uri, getDocumentDiagnostic(document));
+  });
+};
+
+function getDocumentDiagnostic(document: vscode.TextDocument): vscode.Diagnostic[] {
+  const results = detectDocumentWordsRangeandTexts(document);
+  return results.ranges.map((range: vscode.Range, index) => {
+    return {
+      message: results.texts[index],
+      range: range,
+      severity: vscode.DiagnosticSeverity.Information,
+      source: 'Detector'
+    };
+  });
+}
+
+function detectDocumentWordsRangeandTexts(document: vscode.TextDocument): DocumentDetectResult {
+  // check document line by line
+  const lineCount = document.lineCount;
+  const ranges: vscode.Range[] = [];
+  const texts: string[] = [];
+  for (let lineIndex = 0; lineIndex < lineCount; lineIndex++) {
+    const textLine = document.lineAt(lineIndex);
+    if (textLine.text.match(CHINESE_WORDS_REGEX)) {
+      ranges.push(textLine.range);
+      texts.push(textLine.text.match(CHINESE_WORDS_REGEX)![0].toString());
+    }
+  }
+  return { ranges, texts };
+}
+
+
+export const detectChineseWordsinTextEditor = (textEditor: vscode.TextEditor | undefined) => {
+  if (!textEditor) {
+    // Display a message box to the user
+    vscode.window.showInformationMessage('There is no open file!');
     return;
   }
 
   clearDecorations(textEditor);
 
-  // check document line by line
-  const lineCount = textEditor.document.lineCount;
-  const ranges: vscode.Range[] = [];
-  for (let lineIndex = 0; lineIndex < lineCount; lineIndex++) {
-    const textLine = textEditor.document.lineAt(lineIndex);
-    if (CHINESE_WORDS_REGEX.test(textLine.text)) {
-      ranges.push(textLine.range);
-    }
+  const ranges = detectDocumentWordsRangeandTexts(textEditor.document).ranges;
+
+  if (ranges.length === 0) {
+    // no Chinese words found
+    vscode.window.showInformationMessage('No Chinese words found.');
+    return;
   }
 
   // apply decorations to matched lines
@@ -31,4 +91,5 @@ export const detectChineseWords = (textEditor: vscode.TextEditor | undefined) =>
 export const clearDecorations = (textEditor: vscode.TextEditor) => {
   // clear all decorations 
   textEditor.setDecorations(textEditorDecorationTypeHighlight, []);
+  console.log('clear decorations');
 };
